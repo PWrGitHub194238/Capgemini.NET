3be6f6edafc522733f5c3b01db1f6907a6add65b
Tomasz Strzałka <strzalkatomasz@windowslive.com>
2020-11-11 20:27:36 +0100

    Demo 8 start

---
+++ Blazor/Overview/Demo/Capgemini.Net.Blazor.WebAssembly/Capgemini.Net.Blazor.Components.Demo8/Start/ContainerComponent.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Capgemini.Net.Blazor.Components.Demo8.Start
+{
+    public partial class ContainerComponent
+    {
+        [Parameter]
+        public RenderFragment<RateContext> ChildContent { get; set; } = default!;
+
+        [Parameter]
+        public RenderFragment<AverageRateContext> AvgRate { get; set; } = default!;
+
+        private readonly RateContext rateContext = new RateContext();
+
+        private AverageRateContext AverageRateContext => new AverageRateContext()
+        {
+            MinRate = 1,
+            AvgRate = rateContext.AvgRate,
+            MaxRate = rateContext.MaxRate,
+        };
+    }
+}
